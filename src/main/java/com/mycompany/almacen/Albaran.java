a3a2447f6b830d85519ef55fb1af14f1d50b4833
David Velasco <velascobladav@hotmail.com>
2019-12-20 19:13:50 +0100

    Prueba 20_12

--- src/main/java/com/mycompany/almacen/Albaran.java
+++ src/main/java/com/mycompany/almacen/Albaran.java
@@ -4,6 +4,7 @@ package com.mycompany.almacen;
 import java.util.Date;
 
 public class Albaran {
+    //Prueba 20_12
     private Cliente comprador;
     private Date fAlbaran;
     private static int nAlbaran;
