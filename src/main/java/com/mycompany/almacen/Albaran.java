05398724a1ec0ec0d29aa6fea6ab266e728ffd45
Henry <>
2019-12-20 19:22:05 +0100

    Hola

--- src/main/java/com/mycompany/almacen/Albaran.java
+++ src/main/java/com/mycompany/almacen/Albaran.java
@@ -4,7 +4,7 @@ package com.mycompany.almacen;
 import java.util.Date;
 
 public class Albaran {
-    //Prueba 20_12
+    //Prueba 20_12_20
     private Cliente comprador;
     private Date fAlbaran;
     private static int nAlbaran;
