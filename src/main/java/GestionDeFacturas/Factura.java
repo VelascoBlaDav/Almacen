26ac0ce96e6854e03a22917c0bf29f584ae187ed
Henry <enriq@DESKTOP-78V5F6G>
2020-01-12 17:59:50 +0100

    Albaran terminado, Factura empezado, producto modificado un salto de linea

--- src/main/java/GestionDeFacturas/Factura.java
+++ src/main/java/GestionDeFacturas/Factura.java
@@ -8,12 +8,62 @@ import GestionDeProductos.Unidad;
 import java.util.Date;
 
 public class Factura {
-    private Cliente comprador;
+    private String comprador;
     private Date fFactura;
-    private static int nAlbaran;
+    private Albaran[] listaAlbaranes;
     private Unidad[] listaProductos;
     private int total;// ToDo: Calcular en funcion de Unidades
     private String observaciones;
     private formaPago formaPago;
     private estadoFactura estadoFactura;
+
+    public String getComprador() {
+        return comprador;
+    }
+
+    public void setComprador(String comprador) {
+        this.comprador = comprador;
+    }
+
+    public Date getfFactura() {
+        return fFactura;
+    }
+
+    public void setfFactura(Date fFactura) {
+        this.fFactura = fFactura;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public formaPago getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(formaPago formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    public estadoFactura getEstadoFactura() {
+        return estadoFactura;
+    }
+
+    public void setEstadoFactura(estadoFactura estadoFactura) {
+        this.estadoFactura = estadoFactura;
+    }
+    
+    
 }
