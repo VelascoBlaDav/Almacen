46ff64f7df454f3dd5ace14708a86d628b2fdfa9
David Velasco <velascobladav@hotmail.com>
2020-01-12 18:03:10 +0100

    1201_1802

--- src/main/java/GestionDeAlmacenes/Almacen.java
+++ src/main/java/GestionDeAlmacenes/Almacen.java
@@ -8,8 +8,10 @@ import static GestionDeAlmacenes.tipoAlmacen.b;
 import static GestionDeAlmacenes.tipoAlmacen.c;
 
 public class Almacen {
-    //Codigo almacen parametro estatico
+
+    private static int NumAlmacenes;
     private String codAlmacen;
+    private String nombre;
     private tipoAlmacen zona;
     public Unidad[] stock;
     public Unidad[] vendidos;
@@ -20,32 +22,32 @@ public class Almacen {
     private static int numProdB = 0;
     private static int numProdC = 0;
 
-    public Almacen(String codAlmacen, tipoAlmacen zona, Unidad[] stock, Unidad[] vendidos, Unidad[] reservado, Unidad[] libres) {
-        this.codAlmacen = codAlmacen;
+    public Almacen(String nombre, tipoAlmacen zona) {
+        NumAlmacenes++;
+        this.codAlmacen = formatoRefAlmacen(NumAlmacenes);
+        this.nombre=nombre;
         this.zona = zona;
-        this.stock = stock;
-        this.vendidos = vendidos;
-        this.reservado = reservado;
-        this.libres = libres;
-        switch (zona) {
-            case a:
-                numProdA++;
-                break;
-            case b:
-                numProdB++;
-                break;
-            case c:
-                numProdC++;
-                break;
-            default:
-                System.out.println("Error, no existe ese almacen\n");
-                //ToDo crear nuevo almacen
-                break;
-        }
-         
     }
     
-    int i;
+    public static String formatoRefAlmacen(int numeroReferencia){
+        String cadenaReferencia = null;
+        if(1<=numeroReferencia && numeroReferencia<=9){
+            cadenaReferencia="AL-00" + Integer.toString(numeroReferencia);
+        }else if(10<=numeroReferencia && numeroReferencia<=99){
+            cadenaReferencia="AL-0" + Integer.toString(numeroReferencia);
+        }else if(100<=numeroReferencia && numeroReferencia<=999){
+            cadenaReferencia="AL-" + Integer.toString(numeroReferencia);
+        }
+        return cadenaReferencia;
+    } 
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     public String getCodAlmacen() {
         return codAlmacen;
     }
