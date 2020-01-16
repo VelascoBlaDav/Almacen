1712bf902976ae2c2cf5408f3de3f934802e6682
David Velasco <velascobladav@hotmail.com>
2020-01-16 21:18:11 +0100

    Lectura de datos y cambiar almacen modificado

---
+++ src/main/java/Funciones/Lectura.java
@@ -0,0 +1,56 @@
+
+package Funciones;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Lectura {
+    public static String cadena(){
+        Scanner sc = new Scanner (System.in);
+        String cadena=sc.nextLine();
+        return cadena;
+    }
+    public static int entero(){
+        Scanner sc = new Scanner (System.in);
+        String string=sc.nextLine();
+        int entero=0;
+        try {
+            entero=Integer.parseInt(string);
+        } 
+        catch (NumberFormatException ex) 
+        {
+            System.out.println(ex);
+        }
+        return entero;
+    }
+    public static float real(){
+        Scanner sc = new Scanner (System.in);
+        String string=sc.nextLine();
+        float real=0;
+        try {
+            real = Float.parseFloat(string);
+        } 
+        catch (NumberFormatException ex) 
+        {
+            System.out.println(ex);
+        }
+        return real;
+        
+    }
+    public static Date fecha(){
+        Scanner sc = new Scanner (System.in);
+        String string=sc.nextLine();
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        Date fecha = null;
+        try {
+            fecha = formato.parse(string);
+        } 
+        catch (ParseException ex) 
+        {
+            System.out.println(ex);
+        }
+        return fecha;
+    }
+}
--- src/main/java/com/mycompany/almacen/Principal.java
+++ src/main/java/com/mycompany/almacen/Principal.java
@@ -1,8 +1,10 @@
 package com.mycompany.almacen;
 
+import Funciones.Lectura;
 import GestionDeAlmacenes.Almacen;
 import GestionDeAlmacenes.tipoAlmacen;
 import GestionDeClientes.Cliente;
+import GestionDeFacturas.Albaran;
 import GestionDeProductos.Producto;
 import GestionDeProductos.Unidad;
 import GestionDeProductos.estadoProducto;
@@ -11,17 +13,17 @@ import java.util.Date;
 import java.util.Scanner;
 
 
+
 public class Principal {
     private static ArrayList<Almacen> almacenes = new ArrayList<Almacen>();
     private static ArrayList<Producto> productos = new ArrayList<Producto>();
     private static ArrayList<Unidad> unidades = new ArrayList<Unidad>();
     private static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+    private static ArrayList<Albaran> albaranes = new ArrayList<Albaran>();
     
     public static void main(String args[]){
         String referencia = null;
-        int i, salir = 0, si, descuento;
-        Producto p;
-        Almacen a;
+        int i, salir = 0, descuento;
         Unidad u;
         
         //Creamos los tres almacenes de la empresa,1 de cada tipo.
@@ -34,7 +36,7 @@ public class Principal {
         
         do{
             System.out.println("Bienvenido\n");
-            System.out.println("¿Que opcion desea realizar?\n\n");
+            System.out.println("¿Que opción desea realizar?\n\n");
             System.out.println("0)Salir\n"); //Completo
             System.out.println("1)Añadir un nuevo producto\n"); //Completo
             System.out.println("2)Cambiar de almacen un producto\n"); //Completo
@@ -49,29 +51,13 @@ public class Principal {
             System.out.println("11)Añadir unidades de un producto existente\n");//Completo
             System.out.println("12)Añadir descuento a una unidad\n");
 
-            int opcion = sc.nextInt();
+            int opcion = Lectura.entero();
             switch(opcion){
                 case 1:
                     menuAddProducto();
                     break;
                 case 2:
-                    //Buscar el producto
-                    //Seleccionar el nuevo tipo de almacen segun su codigo
-                    //Cambiar el tipo de almacen
-                    p=buscarProducto(referencia);
-                    System.out.println("El almacen del producto es"+p.getAlmacen());
-                    System.out.println("¿Desea cambiarlo?(0-no,1-si)");
-                    sc.nextInt();
-                    si = sc.nextInt();
-                    if(si==1){
-                        System.out.println("Escribe el codigo de referencia del almacen:\n");
-                        sc.nextLine();
-                        referencia = sc.nextLine();
-                        a=buscarAlmacen(referencia);
-                        p.setAlmacen(a);
-                    }else{
-                        break;
-                    }
+                    menuMoverProducto();
                     break;
                 case 3:
                     System.out.println("Escribe el codigo de referencia del almacen:\n");
@@ -175,7 +161,7 @@ public class Principal {
     public static void listarProductos(){
         int i;
         for (i=0;i<productos.size();i++){
-            productos.get(i).toString();
+            System.out.println(productos.get(i).toString());
         }
     }
 //UNIDAD
@@ -224,7 +210,44 @@ public class Principal {
     }
 //ALBARAN
     //BUSCAR ALBARAN
+    public static Albaran buscarAlbaran(String referencia ){
+        for (int i=0;i<albaranes.size();i++){
+            if(albaranes.get(i).getReferencia().equals(referencia)){
+                return albaranes.get(i);
+            }
+        }
+        return null;
+    }
     
+    
+    
+//MENUS
+    public static void menuMoverProducto(){
+        //Buscar el producto
+        //Seleccionar el nuevo tipo de almacen segun su codigo
+        //Cambiar el tipo de almacen
+        Producto p;
+        Almacen a;
+        String referencia;
+        int si;
+        System.out.println("Introduce el código del producto.");
+        referencia=Lectura.cadena();
+        p=buscarProducto(referencia);
+        System.out.println("El almacen del producto es"+p.getAlmacen());
+        System.out.println("¿Desea cambiarlo?:);(0-no,1-si)");
+        System.out.println("0) No.");
+        System.out.println("1) Si.");
+        si = Lectura.entero();
+        if(si==1){
+            System.out.println("Escribe el codigo de referencia del almacen:\n");
+            referencia = Lectura.cadena();
+            a=buscarAlmacen(referencia);
+            p.setAlmacen(a);
+            System.out.println("El almacen ahora es : "+a+".\n");
+        }else{
+            System.out.println("El almacen no se ha modificado.\n");
+        }
+    }
     public static void menuAddCliente(){
         String nombre, nif, direccion;
         float credito;
@@ -232,17 +255,13 @@ public class Principal {
 
         System.out.println("-Añadir nuevo cliente-\n");
         System.out.println("Nombre completo:\n");
-        sc.nextLine();
-        nombre = sc.nextLine();
+        nombre = Lectura.cadena();
         System.out.println("NIF:\n");
-        sc.nextLine();
-        nif = sc.nextLine();
+        nif = Lectura.cadena();
         System.out.println("Dirección (Pais, ciudad, calle, numero, piso, letra):\n");
-        sc.nextLine();
-        direccion = sc.nextLine();
+        direccion = Lectura.cadena();
         System.out.println("Credito (en €):\n");
-        sc.nextFloat();
-        credito = sc.nextFloat();
+        credito = Lectura.real();
 
         addCliente(new Cliente(nombre, nif, direccion, credito));
 
@@ -254,20 +273,15 @@ public class Principal {
 
         System.out.println("-Añadir nuevo producto-\n");
         System.out.println("Escribe el nombre del producto:\n");
-        sc.nextLine();
-        nombreProducto = sc.nextLine();
-        System.out.println("Ancho del producto:\n");
-        sc.nextFloat();
-        ancho = sc.nextFloat();
-        System.out.println("Alto del producto:\n");
-        sc.nextFloat();
-        alto = sc.nextFloat();
-        System.out.println("Precio de compra:\n");
-        sc.nextFloat();
-        pCompra = sc.nextFloat();
-        System.out.println("El codigo de almacen:\n");
-        sc.nextLine();
-        codAlmacen = sc.nextLine();
+        nombreProducto = Lectura.cadena();
+        System.out.println("Ancho del producto (x,x):\n");
+        ancho = Lectura.real();
+        System.out.println("Alto del producto (x,x):\n");
+        alto = Lectura.real();
+        System.out.println("Precio de compra  (en €. x,x):\n");
+        pCompra = Lectura.real();
+        System.out.println("El codigo de almacen (AL-1xxx):\n");
+        codAlmacen = Lectura.cadena();
 
         //Añado el producto al array
         addProducto(new Producto(nombreProducto, ancho, alto, pCompra, codAlmacen));
